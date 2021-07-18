25c5ce9dcb438d24c3eba0851468e03ae3bea027
FilipposKaragiannis <filipkaragiannis@gmail.com>
2021-07-18 23:48:59 +0100

    cp

--- scripts/buildAndDeploy.sh
+++ scripts/buildAndDeploy.sh
@@ -10,7 +10,7 @@ dotnet publish Gram.sln -o publish --no-dependencies
 rm -r "$deployPath"
 mkdir "$deployPath"
 
-find "$pubPath" -type f  \( -iname "Gram.Rpg.*" ! -iname ".deps.json" \) -exec cp "{}" "$deployPath" \;
+find "$pubPath" -type f  \( -iname "Gram.Rpg.*" ! -iname ".deps.json" ! -iname "*.Tests.*" \) -exec cp "{}" "$deployPath" \;
 
 find "$pubPath" -type f  \( -iname "Newtonsoft*" \) -exec cp "{}" "$deployPath" \;
 
